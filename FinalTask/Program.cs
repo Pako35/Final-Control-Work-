3eaf79de7832e40f0b01bc2d938a88da05bedac7
Ortik Israilov <ortik.israilov@gmail.com>
2024-03-25 13:07:34 +0500

    togit

---
+++ FinalTask/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Initial array of strings
+        string[] initialArray = { "long", "one", "two", "short", "three", "longer", "another", "shorty" };
+
+        // Call the FilterStrings method and pass the initial array
+        string[] resultArray = FilterStrings(initialArray);
+
+        // Print the new array to the console
+        Console.WriteLine("Новый массив строк длиной <= 3 символов:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
