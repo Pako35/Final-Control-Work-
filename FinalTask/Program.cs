be8da667aee92cf90a3a9b99cd5f6b9586f1e7dd
Ortik Israilov <ortik.israilov@gmail.com>
2024-03-25 21:54:11 +0500

    ready

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -12,7 +12,35 @@
             }
         }
 
-    // Создаем новый массив нужной длины
+        // Создаем новый массив нужной длины
         string[] outputArray = new string[count];
         int index = 0;
-    
+
+        // Заполняем новый массив строками длиной <= 3 символов
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                outputArray[index] = str;
+                index++;
+            }
+        }
+
+        return outputArray;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterStrings(inputArray);
+
+        Console.WriteLine("Новый массив строк длиной <= 3 символов:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
